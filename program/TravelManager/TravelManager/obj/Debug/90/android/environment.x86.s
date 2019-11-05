9ec095c969844b91b45d89218f8a0e8a1a242979
Nadya <vip.kovcunyak60@gmail.com>
2019-11-05 09:06:35 +0200

    форма реєстрації

--- program/TravelManager/TravelManager/obj/Debug/90/android/environment.x86.s
+++ program/TravelManager/TravelManager/obj/Debug/90/android/environment.x86.s
@@ -61,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"61bf7561-defb-458a-a475-a57a6cb20bfd"
+	.asciz	"7d6bd936-2439-4677-89f8-f03ee8312dbc"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
